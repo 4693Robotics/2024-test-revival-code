9e40e97d6f3e754ef89894030cb181b5faa16196
4693robotics2 <4693robotics@gmail.com>
2024-04-01 15:43:23 -0500

    removed import

--- src/main/java/frc/robot/Commands/ShooterMove.java
+++ src/main/java/frc/robot/Commands/ShooterMove.java
@@ -2,7 +2,6 @@ package frc.robot.Commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.Constants.OIConstants;
